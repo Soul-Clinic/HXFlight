b57ee8671bb7bb5ff4238e28797b107e05a67af2
Can <can@celwk.com>
2014-03-21 02:07:22 +0800

    Intialize

---
+++ HXFlight/PalmHuaxia.h
@@ -0,0 +1,13 @@
+//
+//  PalmHuaxia.h
+//  HXFlight
+//
+//  Created by Can EriK Lu on 3/21/14.
+//  Copyright (c) 2014 Can EriK Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PalmHuaxia : UIViewController
+
+@end
