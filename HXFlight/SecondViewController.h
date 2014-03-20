42f7c103dd87d6adc029b60619029d72377d9c80
Can <can@celwk.com>
2014-03-21 01:04:47 +0800

    Initial Commit

---
+++ HXFlight/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  HXFlight
+//
+//  Created by Can EriK Lu on 3/21/14.
+//  Copyright (c) 2014 Can EriK Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ HXFlight/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  HXFlight
+//
+//  Created by Can EriK Lu on 3/21/14.
+//  Copyright (c) 2014 Can EriK Lu. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
